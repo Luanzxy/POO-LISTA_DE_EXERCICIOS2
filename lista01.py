043d377c1546ef741ec901c83bc8bf07aff55782
Luanzxy <luan.ribeiro@escolar.ifrn.edu.br>
2025-05-06 14:55:27 +0000

    atividade

--- lista01.py
+++ lista01.py
@@ -1,1 +1,27 @@
+# Questão 01
+print("Digite dois valores inteiros:")
+x = int(input())
+y = int(input())
+if x > y:
+    print(f"Maior = {x}")
+elif y > x:
+    print(f"Maior = {y}")
+else:
+    print("Números Iguais")    
+
+
+
+
+
+print("Digite quatro Vaolres inteiros:")
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+#média
+x = (a+b+c+d) // 2
+print(f"Média = {x}")
+
+if a < x:
+    
 
